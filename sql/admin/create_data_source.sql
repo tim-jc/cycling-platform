7317451fdf206446e0b08d06469b76a29421f4bd
tim-jc <65184272+tim-jc@users.noreply.github.com>
2026-06-15 17:33:22 +0100

    Update create_data_source.sql

--- sql/admin/create_data_source.sql
+++ sql/admin/create_data_source.sql
@@ -1,16 +1,13 @@
-CREATE TABLE IF NOT EXISTS admin.etl_run (
+CREATE TABLE IF NOT EXISTS admin.data_source (
 
-    run_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    source_id INT AUTO_INCREMENT PRIMARY KEY,
 
-    run_started DATETIME NOT NULL,
-    run_completed DATETIME NULL,
+    source_name VARCHAR(50) NOT NULL,
 
-    run_status VARCHAR(20) NOT NULL,
+    source_description VARCHAR(255) NULL,
 
-    run_mode VARCHAR(20) NOT NULL,
+    is_active BOOLEAN NOT NULL,
 
-    source_system VARCHAR(50) NOT NULL,
-
-    inserted_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at DATETIME NOT NULL
 
 );
