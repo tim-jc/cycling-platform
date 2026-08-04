6f369a101dfdbc7d28c08deeaf7881ea381e0cfb
tim-jc <tim_cross@me.com>
2026-08-04 11:09:19 +0100

    platform.R has been replaced by run_raw_ingestion.R this compatibility wrapper now deleted following successful scheduled runs

--- platform.R
+++
@@ -1,8 +0,0 @@
-# Temporary compatibility wrapper.
-warning(
-  "platform.R is deprecated; use run_raw_ingestion.R instead.",
-  call. = FALSE,
-  immediate. = TRUE
-)
-
-source("run_raw_ingestion.R")
